60fbb7f548e89dcff0a738b6ac80d05ae9f86bb0
lxizaxi <USERNAME@users.noreply.github.com>
2024-05-21 21:56:57 +0900

    sample動かした

---
+++ sample.py
@@ -0,0 +1,29 @@
+from datetime import timedelta
+from os import environ
+from typing import Final
+
+import amplify
+import dotenv
+from amplify import FixstarsClient, VariableGenerator
+
+dotenv.load_dotenv(override=True)
+
+FIXSTARS_TOKEN: Final[str] = environ.get("FIXSTARS_TOKEN", "")
+
+
+# 1. 変数配列の作成
+gen = VariableGenerator()
+q = gen.array("Binary", 2)
+
+# 2. 目的関数の作成
+f = q[0] * q[1] + q[0] - q[1] + 1
+
+# 3. ソルバークライアントの作成
+client = FixstarsClient()
+client.token = FIXSTARS_TOKEN
+client.parameters.timeout = timedelta(milliseconds=1000)
+
+# 4. ソルバーの実行
+result = amplify.solve(model=f, client=client)
+
+print(result)
